290599cf2716f3af90ab6ae9d08dbccc37f7bddb
Kamil Anindita <55023703+kamilanindita@users.noreply.github.com>
2020-06-29 19:20:54 +0700

    add route in now.js

--- now.js
+++ now.js
@@ -1,4 +1,21 @@
 {
   "version": 2,
   "builds": [{ "src": "index.js", "use": "@now/node-server" }],
+  "routes": [
+    {
+      "src": "/",
+      "dest": "/index.js",
+      "methods": ["GET"]
+    },
+    {
+      "src": "/user",
+      "dest": "/index.js",
+      "methods": ["GET"]
+    },
+    {
+      "src": "/product",
+      "dest": "/index.js",
+      "methods": ["GET"]
+    }
+  ]
 }
